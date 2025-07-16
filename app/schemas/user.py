60a1f0bb22b93d2fd43080a15086eaf082582943
Rumjot Singh <155811126+rumjotsingh@users.noreply.github.com>
2025-07-16 12:10:28 +0530

    Update user.py

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -13,10 +13,17 @@ class UserOut(BaseModel):
     email: EmailStr
     name: str
     role: str
-
 class Token(BaseModel):
     access_token: str
     token_type: str = "bearer"
+class UserProfile(BaseModel):
+    email: str
+    name: str
+    role: str
+
+class TokenWithUser(BaseModel):
+    access_token: str
+    user: UserProfile
 
 class UserUpdate(BaseModel):
     email: Optional[EmailStr] = None
